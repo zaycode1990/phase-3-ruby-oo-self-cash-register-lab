51e2ebcdc6deea5c5c93fe3ba7632cb8c23dcfdf
Isaiah <ijandicos90@gmail.com>
2022-07-20 13:06:21 -0400

    commit tree 02d0b6f35e7bfa7a4a1eb42a966c958346915d1a

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,19 +1,23 @@
+require "pry"
+
 class CashRegister
   attr_accessor :items, :discount, :total, :previous_transaction
 
   def initialize(discount = 0)
-    @total = 0
-    @discount = discount
-    @items = []
+    total = 0
+    discount = discount
+    items = []
   end
 
   def add_item(title, price, quantity = 1)
+    binding.pry
     self.previous_transaction = price * quantity
     self.total += self.previous_transaction
     quantity.times { self.items << title }
   end
 
   def apply_discount
+    binding.pry
     if self.discount != 0
       percentage_discount = (100.0 - self.discount.to_f) / 100
       self.total = (self.total * percentage_discount).to_i
